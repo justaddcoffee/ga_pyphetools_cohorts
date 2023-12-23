727675310b9d7ba330e1a17709d9333b1b0c695a
Justin Reese <justaddcoffee@gmail.com>
2023-12-23 16:31:41 -0500

    Lint

--- tests/test_ga.py
+++ tests/test_ga.py
@@ -4,7 +4,6 @@ from ga.utils.phenopacket import parse_phenopackets
 
 
 class TestGA(TestCase):
-
     @classmethod
     def setUpClass(cls) -> None:
         pass
@@ -14,24 +13,41 @@ class TestGA(TestCase):
 
     def test_parse_phenopackets(self):
         # test/data is a directory of with 1 phenopacket
-        p = parse_phenopackets('tests/data/')
+        p = parse_phenopackets("tests/data/")
         self.assertIsInstance(p, dict)
         self.assertEqual(len(list(p.keys())), 2)
-        self.assertCountEqual(p.keys(), {'phenotype_data', 'all_data'})
-        self.assertTrue('Craniometaphyseal dysplasia' in p['phenotype_data'].keys())
-        self.assertTrue('Craniometaphyseal dysplasia' in p['all_data'].keys())
+        self.assertCountEqual(p.keys(), {"phenotype_data", "all_data"})
+        self.assertTrue("Craniometaphyseal dysplasia" in p["phenotype_data"].keys())
+        self.assertTrue("Craniometaphyseal dysplasia" in p["all_data"].keys())
 
-        self.assertTrue('PMID_20186813_6-year-old_male_patient' in p['phenotype_data']['Craniometaphyseal dysplasia'].keys())
-        self.assertTrue('PMID_20186813_6-year-old_male_patient' == p['all_data']['Craniometaphyseal dysplasia'][0]['id'])
+        self.assertTrue(
+            "PMID_20186813_6-year-old_male_patient"
+            in p["phenotype_data"]["Craniometaphyseal dysplasia"].keys()
+        )
+        self.assertTrue(
+            "PMID_20186813_6-year-old_male_patient"
+            == p["all_data"]["Craniometaphyseal dysplasia"][0]["id"]
+        )
 
-        self.assertEqual(len(p['phenotype_data']['Craniometaphyseal dysplasia']['PMID_20186813_6-year-old_male_patient']), 7)
-        self.assertCountEqual(p['phenotype_data']['Craniometaphyseal dysplasia']['PMID_20186813_6-year-old_male_patient'],
-                          [ ('HP:0000407', 'Sensorineural hearing impairment', 'observed'),
-                                    ('HP:0000572', 'Visual loss', 'observed'),
-                                    ('HP:0011856', 'Pica', 'observed'),
-                                    ('HP:0100255', 'Metaphyseal dysplasia', 'observed'),
-                                    ('HP:0100774', 'Hyperostosis', 'observed'),
-                                    ('HP:0003196', 'Short nose', 'observed'),
-                                    ('HP:0011856', 'Pica', 'observed')])
-
-
+        self.assertEqual(
+            len(
+                p["phenotype_data"]["Craniometaphyseal dysplasia"][
+                    "PMID_20186813_6-year-old_male_patient"
+                ]
+            ),
+            7,
+        )
+        self.assertCountEqual(
+            p["phenotype_data"]["Craniometaphyseal dysplasia"][
+                "PMID_20186813_6-year-old_male_patient"
+            ],
+            [
+                ("HP:0000407", "Sensorineural hearing impairment", "observed"),
+                ("HP:0000572", "Visual loss", "observed"),
+                ("HP:0011856", "Pica", "observed"),
+                ("HP:0100255", "Metaphyseal dysplasia", "observed"),
+                ("HP:0100774", "Hyperostosis", "observed"),
+                ("HP:0003196", "Short nose", "observed"),
+                ("HP:0011856", "Pica", "observed"),
+            ],
+        )
