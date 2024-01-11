d8f60f99fab90418d04f1eb648138ce9bab704fe
Justin Reese <justaddcoffee@gmail.com>
2024-01-11 15:54:51 -0500

    Start adding make phenopacket command

---
+++ src/ga/cli_make_synthetic_data.py
@@ -0,0 +1,7 @@
+import click
+import phenotype2phenopacket
+
+@click.command("optimize-hyperparameters")
+def make_synthetic_data_command():
+    """Optimize hyperparameters for the genetic algorithm."""
+    print("lorem ipsum")
