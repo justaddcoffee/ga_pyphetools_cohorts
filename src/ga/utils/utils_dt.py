b8b967101053c83501ea8a43e455df5502ba9e1f
ele <elena.casiraghi@unimi.it>
2024-02-24 08:06:32 +0100

    removed old and unused function vertical_to_horizontal from utils_dt and utils_rf; the function is now in file src/ga/utils/vertical-to_horizontal.py and its is called from cli_run_all.py

--- src/ga/utils/utils_dt.py
+++ src/ga/utils/utils_dt.py
@@ -33,20 +33,6 @@ def my_aucpr_scorer(y, y_pred, **kwargs):
     return auc_score
 
 
-def vertical_to_horizontal(df):
-    print(df.columns)
-
-    df = df.drop_duplicates()
-
-    # Group by "person_id" and "patient_label" and pivot on "hpo_term_id"
-    df_pivoted = df.drop(columns=['hpo_term_label', 'negated', 'weight']).groupby(
-        ["person_id", "patient_label", "hpo_term_id"]).size().unstack(fill_value=0).reset_index()
-
-    print(df_pivoted.columns)
-
-    return df_pivoted
-
-
 def run_dt_algorithm(df_train: pd.DataFrame,
                      df_test: pd.DataFrame,
                      disease: str,
